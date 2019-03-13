eb06e271b6ab1ab8083aaf1e2f967dd8ac68d661
caro <carlankjaer@gmail.com>
2019-03-13 16:04:02 +0100

    Initial commit

---
+++ SokobanProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SokobanProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
